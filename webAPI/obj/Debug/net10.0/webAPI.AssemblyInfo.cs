51ff97775550a4dc253a5ce3c545ce524deac84f
majito <mariajoseportill2@gmail.com>
2026-05-09 15:39:18 -0600

    docker

--- webAPI/obj/Debug/net10.0/webAPI.AssemblyInfo.cs
+++ webAPI/obj/Debug/net10.0/webAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("webAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c6436ec3e4861797c2497937c5b066f61d50b508")]
 [assembly: System.Reflection.AssemblyProductAttribute("webAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("webAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
